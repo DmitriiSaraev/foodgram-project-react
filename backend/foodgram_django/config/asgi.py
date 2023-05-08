099e22041b8ef3564fc669ac3f8f8ff6436a1701
unknown <saraevdmitrii@gmail.com>
2023-05-08 21:51:08 +0500

    com

--- backend/foodgram_django/config/asgi.py
+++ backend/foodgram_django/config/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI foodgram_django for foodgram_django project.
+ASGI config for config project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodgram_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_asgi_application()
---
+++ backend/foodgram_django/config/settings.py
@@ -0,0 +1,472 @@
+# import os
+# from pathlib import Path
+# from dotenv import load_dotenv
+#
+# # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# BASE_DIR = Path(__file__).resolve().parent.parent
+#
+# load_dotenv()
+#
+# # Quick-start development settings - unsuitable for production
+# # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+#
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = (
+#     'django-insecure-*y@3h=^&d*!rmgt=^&$_&qervu)+hep-#gslpn86mzu_*ve+*p'
+# )
+#
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+#
+# # ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = [
+#     "localhost",
+#     "127.0.0.1",
+#     "[::1]",
+#     "testserver",
+# ]
+#
+#
+# # Application definition
+#
+# INSTALLED_APPS = [
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+#
+#     'rest_framework',
+#     'rest_framework.authtoken',
+#     'djoser',
+#     'django_filters',
+#
+#     'users',
+#     'recipes',
+#     'api',
+# ]
+#
+# MIDDLEWARE = [
+#     'django.middleware.security.SecurityMiddleware',
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+# ]
+#
+# ROOT_URLCONF = 'config.urls'
+#
+# # TEMPLATES_DIR = BASE_DIR / 'templates'
+# # 'DIRS': [TEMPLATES_DIR, ],
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#         },
+#     },
+# ]
+#
+# # TEMPLATES = [
+# # #     {
+# # #         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+# # #         'DIRS': [],
+# # #         'APP_DIRS': True,
+# # #         'OPTIONS': {
+# # #             'context_processors': [
+# # #                 'django.template.context_processors.debug',
+# # #                 'django.template.context_processors.request',
+# # #                 'django.contrib.auth.context_processors.auth',
+# # #                 'django.contrib.messages.context_processors.messages',
+# # #             ],
+# # #         },
+# # #     },
+# # # ]
+#
+# WSGI_APPLICATION = 'config.wsgi.application'
+#
+#
+# # Database
+# # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+#
+# # DATABASES = {
+# #     'default': {
+# #         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+# #         'NAME': os.getenv('DB_NAME'),
+# #         'USER': os.getenv('POSTGRES_USER'),
+# #         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+# #         'HOST': os.getenv('DB_HOST'),
+# #         'PORT': os.getenv('DB_PORT')
+# #     }
+# # }
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+#
+#
+# # Password validation
+# # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+#
+# AUTH_PASSWORD_VALIDATORS = [
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#     },
+# ]
+#
+#
+# # Internationalization
+# # https://docs.djangoproject.com/en/3.2/topics/i18n/
+#
+# LANGUAGE_CODE = 'ru'
+#
+# TIME_ZONE = 'UTC'
+#
+# USE_I18N = True
+#
+# USE_L10N = True
+#
+# USE_TZ = True
+#
+#
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/3.2/howto/static-files/
+#
+# # STATIC_URL = '/static/'
+# #
+# # MEDIA_URL = '/media/'
+# # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# #
+# #
+# # STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
+# #
+# # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# #
+# # AUTH_USER_MODEL = 'users.User'
+#
+# STATIC_URL = '/static/'
+#
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#
+#
+# REST_FRAMEWORK = {
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.IsAuthenticated',
+#     ],
+#     'DEFAULT_AUTHENTICATION_CLASSES': [
+#         'rest_framework.authentication.TokenAuthentication',
+#     ],
+#     'DEFAULT_PAGINATION_CLASS':
+#         'rest_framework.pagination.LimitOffsetPagination',
+#
+#     'PAGE_SIZE': 6,
+#
+#     'DEFAULT_FILTER_BACKENDS': [
+#         'django_filters.rest_framework.DjangoFilterBackend'
+#     ],
+# }
+#
+#
+# DJOSER = {
+#     'SERIALIZERS': {
+#         'user': 'api.serializers.UserSerializer',
+#         'user_create': 'api.serializers.UserCreateMySerializer',
+#         'current_user': 'api.serializers.UserSerializer'},
+#     'LOGIN_FIELD': 'email',
+#     'PERMISSIONS': {
+#         'user': ['rest_framework.permissions.AllowAny', ],
+#         'user_list': ['rest_framework.permissions.AllowAny', ]
+#     },
+#     'HIDE_USERS': False,
+# }
+
+import os
+
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Quick-start development settings - unsuitable for production
+
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+
+SECRET_KEY = 'django-insecure-*y@3h=^&d*!rmgt=^&$_&qervu)+hep-#gslpn86mzu_*ve+*p'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+# Application definition
+
+
+INSTALLED_APPS = [
+
+    'django.contrib.admin',
+
+    'django.contrib.auth',
+
+    'django.contrib.contenttypes',
+
+    'django.contrib.sessions',
+
+    'django.contrib.messages',
+
+    'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'rest_framework.authtoken',
+
+    'djoser',
+
+    'django_filters',
+
+    'users',
+
+    'recipes',
+
+    'api',
+
+]
+
+MIDDLEWARE = [
+
+    'django.middleware.security.SecurityMiddleware',
+
+    'django.contrib.sessions.middleware.SessionMiddleware',
+
+    'django.middleware.common.CommonMiddleware',
+
+    'django.middleware.csrf.CsrfViewMiddleware',
+
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+
+    'django.contrib.messages.middleware.MessageMiddleware',
+
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+]
+
+ROOT_URLCONF = 'config.urls'
+
+TEMPLATES_DIR = BASE_DIR / 'templates'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# TEMPLATES = [
+
+#     {
+
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+
+#         'DIRS': [],
+
+#         'APP_DIRS': True,
+
+#         'OPTIONS': {
+
+#             'context_processors': [
+
+#                 'django.template.context_processors.debug',
+
+#                 'django.template.context_processors.request',
+
+#                 'django.contrib.auth.context_processors.auth',
+
+#                 'django.contrib.messages.context_processors.messages',
+
+#             ],
+
+#         },
+
+#     },
+
+# ]
+
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+# Database
+
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+
+if DEBUG is True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+            'NAME': os.getenv('DB_NAME', default='postgres'),
+            'USER': os.getenv('POSTGRES_USER', default='postgres'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+            'HOST': os.getenv('DB_HOST', default='db'),
+            'PORT': os.getenv('DB_PORT', default=5432)
+        }
+    }
+
+# Password validation
+
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+
+AUTH_PASSWORD_VALIDATORS = [
+
+    {
+
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+
+    },
+
+    {
+
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+
+    },
+
+    {
+
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+
+    },
+
+    {
+
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+
+    },
+
+]
+
+# Internationalization
+
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+
+
+LANGUAGE_CODE = 'ru'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_PERMISSION_CLASSES': [
+
+        'rest_framework.permissions.IsAuthenticated',
+
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+
+        'rest_framework.authentication.TokenAuthentication',
+
+    ],
+
+    'DEFAULT_PAGINATION_CLASS':
+
+        'rest_framework.pagination.LimitOffsetPagination',
+
+    'PAGE_SIZE': 6,
+
+    'DEFAULT_FILTER_BACKENDS': [
+
+        'django_filters.rest_framework.DjangoFilterBackend'
+
+    ],
+
+}
+
+DJOSER = {
+
+    'SERIALIZERS': {
+
+        'user': 'api.serializers.UserSerializer',
+
+        'user_create': 'api.serializers.UserCreateMySerializer',
+
+        'current_user': 'api.serializers.UserSerializer'},
+
+    'LOGIN_FIELD': 'email',
+
+    'PERMISSIONS': {
+
+        'user': ['rest_framework.permissions.AllowAny', ],
+
+        'user_list': ['rest_framework.permissions.AllowAny', ]
+
+    },
+
+    'HIDE_USERS': False,
+
+}
