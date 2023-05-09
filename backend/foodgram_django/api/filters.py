70cba5a7f3067426a5944681bf785cd6f2f839ad
unknown <saraevdmitrii@gmail.com>
2023-05-09 21:29:55 +0500

    com

--- backend/foodgram_django/api/filters.py
+++ backend/foodgram_django/api/filters.py
@@ -9,7 +9,7 @@ class CharFilterInFilter(filters.BaseInFilter, filters.CharFilter):
 class RecipeFilter(filters.FilterSet):
     author = filters.CharFilter(field_name="author__id")
     # tag = CharFilterInFilter(field_name="tags__slug", lookup_expr="in")
-    tag = filters.ModelMultipleChoiceFilter(
+    tags = filters.ModelMultipleChoiceFilter(
         queryset=Tag.objects.all(),
         field_name='tags__slug',
         to_field_name='slug',
@@ -21,7 +21,7 @@ class RecipeFilter(filters.FilterSet):
 
     class Meta:
         model = Recipe
-        fields = ["is_favorited", "author", "is_in_shopping_cart", "tag"]
+        fields = ["is_favorited", "author", "is_in_shopping_cart", "tags"]
 
     def get_is_favorited(self, queryset, name, value):
         if value:
